2724b0498e082596d05a2a0308eb1b9e12b7bbd5
s-a <stephan.ahlf@googlemail.com>
2015-06-06 10:07:15 +0200

    switched to python build process

---
+++ build/build.py
@@ -0,0 +1,11 @@
+import fontforge
+font = fontforge.open("../src/digital-numbers.sfd")
+for glyph in font.glyphs():
+    glyph.round()
+    #glyph.correctDirection()
+    glyph.removeOverlap()
+    glyph.addExtrema()
+    glyph.simplify()
+formats = ['../dist/sa-digital-number.ttf', '../dist/sa-digital-number.svg', '../dist/sa-digital-number.woff', '../dist/sa-digital-number.eot']
+for file in formats:
+    font.generate(file, flags=("round", "dummy-dsig"))
--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.ttf
-fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.svg
-fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.woff
-fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.eot
+fontforge -lang=py -script ./build.py
+# fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.ttf
+# fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.svg
+# fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.woff
+# fontforge -script script.pe ../src/digital-numbers.sfd ../dist/sa-digital-number.eot
 cp ../dist/sa-digital-number.* ../demo/fonts/
